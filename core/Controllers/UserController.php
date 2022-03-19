bd7071b40d6c4edd59428955da953e9f620b8f9e
lamalilow <ya.felip@yandex.ru>
2022-03-19 13:06:19 +0500

    version 1.0.7

--- core/Controllers/UserController.php
+++ core/Controllers/UserController.php
@@ -13,13 +13,27 @@ class UserController extends BaseController
 
     public function registerPost()
     {
-        if($_POST['password'] != $_POST['password_confirmed'])
-            return header('Location: ?error_password');
-        unset($_POST['password_confirmed']);
-        $user = new User();
+        $errors = [];
+        if(!isset($_POST['name'])) $errors['name'][] = 'Нет поля name';
+        if(!isset($_POST['login'])) $errors['login'][] = 'Нет поля login';
+        if(!isset($_POST['password'])) $errors['password'][] = 'Нет поля password';
+        if(!isset($_POST['password_confirmed'])) $errors['password_confirmed'][] = 'Нет поля password_confirmed';
 
-        $findUser = $user->create($_POST);
-        return var_dump($findUser);
-        //return view('index');
+        if(empty($_POST['name'])) $errors['name'][] = 'Поле name не заполнено!';
+        if(empty($_POST['login'])) $errors['login'][] = 'Поле login не заполнено!';
+        if(empty($_POST['password'])) $errors['password'][] = 'Поле password не заполнено!';
+        if(empty($_POST['password_confirmed'])) $errors['password_confirmed'][] = 'Поле password_confirmed не заполнено!';
+
+        if($_POST['password'] != $_POST['password_confirmed']) $errors['password'][] = 'Пароли не совпали!';
+
+        if($errors != [])
+            return view('register', compact('errors'));
+
+        unset($_POST['password_confirmed']);
+        # Шифруем пароль в SHA1
+        $_POST['password'] = sha1($_POST['password']);
+        $user = new User();
+        $user = $user->create($_POST);
+        return view('users/successRegister', compact('user'));
     }
 }
